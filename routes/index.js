84dc1a678d35e98ac12bedf33ac29db856ee72ca
Alan Taylor <alancarltaylor@gmail.com>
2016-03-25 10:23:29 -0600

    hipstafy in da house

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var snippets = require('../lib/snippets')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next){
+  if (req.body.phrase){
+        var userInputSplit = req.body.phrase.split(/\s/g);
+        var hipstaphrase = "";
+        for (var i = 0; i < userInputSplit.length; i ++){
+          hipstaphrase += userInputSplit[i] + " " + snippets[(Math.floor(Math.random() * snippets.length))] + "<br>"
+         }
+        res.render('index', { phrase: hipstaphrase})
+      } else {
+    res.render('index', {error: "Nothing provided"});
+  }
+});
+
 
 module.exports = router;
